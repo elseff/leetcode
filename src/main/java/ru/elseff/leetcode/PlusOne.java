a022664e8513c81455f259b2e9a4553bdbba49b5
elseff <danilapetrov522@gmail.com>
2023-07-25 21:21:20 +0400

    plus one

---
+++ src/main/java/ru/elseff/leetcode/PlusOne.java
@@ -0,0 +1,28 @@
+package ru.elseff.leetcode;
+
+import java.util.Arrays;
+
+public class PlusOne {
+    public static void main(String[] args) {
+        int[] digits = {9, 9};
+        System.out.println(Arrays.toString(plusOne(digits)));
+    }
+
+    public static int[] plusOne(int[] digits) {
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i]<9){
+                digits[i] += 1;
+                return digits;
+            }
+            digits[i]=0;
+        }
+        digits = new int[digits.length+1];
+        digits[0]=1;
+        return digits;
+    }
+
+    private static boolean incrementAndGetNeedleOfTransferUnit(int[] digits, int index) {
+        digits[index] = digits[index] + 1;
+        return digits[index] == 10;
+    }
+}
